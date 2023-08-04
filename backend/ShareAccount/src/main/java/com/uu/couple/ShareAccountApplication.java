e3a9c633bb03ccc3d831da3d3c324b7c7320aede
유영 <dbdud1999@naver.com>
2023-08-05 01:40:37 +0900

    (유영) INIT: 스켈레톤 코드 생성

---
+++ backend/ShareAccount/src/main/java/com/uu/couple/ShareAccountApplication.java
@@ -0,0 +1,13 @@
+package com.uu.couple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShareAccountApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShareAccountApplication.class, args);
+	}
+
+}
