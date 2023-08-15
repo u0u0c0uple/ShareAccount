43248687a0cc741b89fdff716fc6ab8c015e3a95
유영 <dbdud1999@naver.com>
2023-08-15 18:20:08 +0900

    (유영) FIX: Group Entity에 Base Entity extend

--- backend/ShareAccount/src/main/java/com/uu/couple/model/entity/Group.java
+++ backend/ShareAccount/src/main/java/com/uu/couple/model/entity/Group.java
@@ -9,6 +9,8 @@ import org.hibernate.annotations.Comment;
 import org.hibernate.annotations.DynamicInsert;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
 
 @Entity
 @Table(indexes = {
@@ -20,26 +22,29 @@ import java.time.LocalDateTime;
 @Builder
 @Getter
 @DynamicInsert
-public class Group {
+public class Group extends Base {
     @Id
-    private String id;
-
-    @Column(name="name", nullable = false)
-    @Comment("지출 장소")
-    private String name;
-
-    @Column(name="location", nullable = false)
-    @Comment("지출 장소 주소")
-    private String location;
+    private String id = UUID.randomUUID().toString();  // UUID 자동 생성
 
     /* 추후 추가 예정
         @ManyToOne(fetch = FetchType.LAZY)
-        @JoinColumn(name="category_id", nullable = false)
+        @JoinColumn(name = "category_id", nullable = false)
         @Comment("분류")
         private Category category;
     */
 
-    @Column(name="spend_date", nullable = false)
+    @Column(name = "name", nullable = false)
+    @Comment("지출 장소")
+    private String name;
+
+    @Column(name = "location", nullable = false)
+    @Comment("지출 장소 주소")
+    private String location;
+
+    @OneToMany(mappedBy = "group")
+    List<Spend> spendList;
+
+    @Column(name = "spend_date", nullable = false)
     @Comment("지출 날짜")
     private LocalDateTime spendDate;  // YYYY.MM.DD HH:SS
 
