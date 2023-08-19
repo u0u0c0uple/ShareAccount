4258b9d900ece97669942138b231b9d367767642
l0u0h0 <l0u0h0@gmail.com>
2023-08-20 00:41:20 +0900

    (유한) FEAT: 라우팅 설정 App.tsx에 설정, Main페이지 생성, 에러컴포넌트 생성, 디자인 패턴 구조 생성

---
+++ frontend/ShareAccount/src/common/ErrorComponent.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { FallbackProps } from 'react-error-boundary';
+
+const ErrorComponent: React.FC<FallbackProps> = ({ error }) => {
+  if (error == undefined) {
+    return <div>알 수 없는 오류 발생</div>
+  }
+  return <div>{error.message}</div>
+}
+
+export default ErrorComponent;
