69d64889964335b115935785a61faf1737d15f6c
You Young <dbdud1999@naver.com>
2023-08-22 11:38:24 +0900

    (유영) :sparkles: Spend Create API 생성 완료

--- backend/ShareAccount/src/main/java/com/uu/couple/controller/SpendController.java
+++ backend/ShareAccount/src/main/java/com/uu/couple/controller/SpendController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,8 +24,9 @@ public class SpendController {
     }
 
     @PostMapping("create")
-    public ResponseEntity<?> setSpend(PlaceRequestDto placeRequestDto) {
+    public ResponseEntity<?> setSpend(@RequestBody PlaceRequestDto placeRequestDto) {
         try {
+            System.out.println(placeRequestDto);
             boolean setSpendResponse = spendService.setSpend(placeRequestDto);
 
             if(!setSpendResponse) throw new SetSpendException();
--- backend/ShareAccount/src/main/java/com/uu/couple/model/service/SpendServiceImpl.java
+++ backend/ShareAccount/src/main/java/com/uu/couple/model/service/SpendServiceImpl.java
@@ -1,16 +1,37 @@
 package com.uu.couple.model.service;
 
 import com.uu.couple.model.dto.request.PlaceRequestDto;
+import com.uu.couple.model.dto.request.SpendRequestDto;
+import com.uu.couple.model.entity.Place;
 import com.uu.couple.model.entity.Spend;
+import com.uu.couple.model.respository.PlaceRepository;
+import com.uu.couple.model.respository.SpendRepository;
 import com.uu.couple.util.SetSpendException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class SpendServiceImpl implements SpendService {
+
+    private final PlaceRepository placeRepository;
+    private final SpendRepository spendRepository;
+
+    @Autowired
+    private SpendServiceImpl(PlaceRepository placeRepository, SpendRepository spendRepository) {
+        this.placeRepository = placeRepository;
+        this.spendRepository = spendRepository;
+    }
+
     public boolean setSpend(PlaceRequestDto placeRequestDto) throws SetSpendException {
+        System.out.println(placeRequestDto);
         /*** 유효성검사 ***/
         String name = placeRequestDto.getName();
         if(name == null || name.trim().equals("") || 20 < name.length()) {  // 지출 장소가 입력되지 않았을 경우 혹은 20자보다 길 경우
@@ -22,21 +43,39 @@ public class SpendServiceImpl implements SpendService {
             throw new SetSpendException("지출 장소의 주소가 잘못 입력 되었습니다.");
         }
 
-        List<Spend> spendList = placeRequestDto.getSpendList();
-        if(spendList == null || spendList.size() == 0) {  // 지출 내역이 입력되지 않았을 경우
+        List<SpendRequestDto> spendRequestList = placeRequestDto.getSpendList();
+        if(spendRequestList == null || spendRequestList.size() == 0) {  // 지출 내역이 입력되지 않았을 경우
             throw new SetSpendException("지출 내역이 잘못 입력 되었습니다.");
-        }
-
-        LocalDateTime spendDate = placeRequestDto.getSpendDate();
-        if(spendDate == null) {  // 지출 날짜가 입력되지 않았을 경우
-            throw new SetSpendException("지출 날짜가 잘못 입력 되었습니다.");
         }
         
         try {
-            /*
-                TO DO :: 생성 내역 DB 저장
-             */
+            String spendDateStr = placeRequestDto.getSpendDate();
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+            LocalDateTime spendDate = LocalDateTime.parse(spendDateStr, formatter);
+
+            Place place = Place.builder()
+                    .id(UUID.randomUUID().toString())  // UUID 생성
+                    .name(name)
+                    .location(location)
+                    .spendDate(spendDate)
+                    .build();
+
+            placeRepository.save(place);
+
+            List<Spend> spendList = new ArrayList<>();
+            for (SpendRequestDto spend : spendRequestList) {  // 각 지출 목록 저장
+                spendList.add(Spend.builder()
+                        .id(UUID.randomUUID().toString())  // UUID 생성
+                        .name(spend.getName())
+                        .amount(spend.getAmount())
+                        .adjust(spend.isAdjust())
+                        .place(place)
+                        .build());
+            }
+            spendRepository.saveAll(spendList);
             return true;
+        } catch (DateTimeParseException e) {
+            throw new SetSpendException("날짜가 잘못 입력 되었습니다.");
         } catch(Exception e) {
             e.printStackTrace();
             throw new SetSpendException();
