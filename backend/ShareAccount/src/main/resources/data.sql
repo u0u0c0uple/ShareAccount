9bdfec6299833a089801e7513f10731b1a384b1c
유영 <dbdud1999@naver.com>
2023-08-27 21:44:30 +0900

    (유영) :sparkles: dump data 자동 삽입

---
+++ backend/ShareAccount/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO `couple` (`id`, `name`, `start_date`, ) VALUES ('uu_couple', '유유커플', '2023-04-01 00:00:00.00');
+INSERT INTO `user` (`id`, `name`, `nickname`, `couple_id`) VALUES ('l0u0h0', '이유한', '유한찌', 'uu_couple');
+INSERT INTO `user` (`id`, `name`, `nickname`, `couple_id`) VALUES ('dbdud1999', '유영', '영찌', 'uu_couple');
